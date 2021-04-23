8aa5176d9e510e381af011db8718ac3816246173
Vorfall <alekseevegorc@gmail.com>
2021-04-23 11:13:12 +0300

    Start Task Happy Array

--- src/main/kotlin/main.kt
+++ src/main/kotlin/main.kt
@@ -1,19 +1,14 @@
 fun main(args: Array<String>) {
+
     val input = intArrayOf(13, 2, 36, 48, 59, 0, 120)
     val result = intArrayOf(158, 278)
     val res = intArrayOf(0, 0)
-    var max=0
-    var min=0
-
     input.sort()
-
-    for(i in 0..5)
+    for(i in 0..(input.size-2))
     {
         res[0]+=input[i];
-        res[1]+=input[input.size-1-i]
+        res[1]+=input[input.size-i-1]
     }
-
-
     println(res[0])
     println(res[1])
 }
